59c6d0abf5009fec65d9d5867cb0db67eecccf80
Tom Voss <voss@icomcept.de>
2021-04-23 13:22:19 +0200

    fix ip number change handling

--- modules/util/solaredge_util.py
+++ modules/util/solaredge_util.py
@@ -1,17 +1,23 @@
-import os.path
+import os
 from pymodbus.client.sync import ModbusTcpClient, ConnectionException
 
 def getPortNo(ipaddress):
     slaveid = 1
+    file_path = '/var/www/html/openWB/ramdisk/solaredgeport'
     # ModBus is configured to listen on port 502 or 1502, actual port is autodetected and cache in ramdisk
     port = 0                    # port not defined
     ramdiskInitialized = False  # assume ramdisk cache is not set
-    if os.path.isfile('/var/www/html/openWB/ramdisk/solaredgeport'):
-        with open('/var/www/html/openWB/ramdisk/solaredgeport', 'r') as f:
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as f:
             try:
-                port = int(f.read())
-                ramdiskInitialized = True
-                return port
+                data = f.read()
+                (ipaddr, port) = data.split(':')
+                if(ipaddress == ipaddr):
+                    ramdiskInitialized = True
+                    return port
+                else:
+                    print('changed ip addr, old: %s, new: %s' % (ipaddr, ipaddress))
+                    os.remove(file_path)
             except:
                 pass    # ramdisk file does not exist, proceed with autodetection
 
@@ -22,17 +28,22 @@ def getPortNo(ipaddress):
         resp= client.read_holding_registers(40206,5,unit=slaveid)
         if not ramdiskInitialized:  # if we reach this line, we were able to read ModBus registers
             # ramdisk is not set, so persist port value now for next time
-            with open('/var/www/html/openWB/ramdisk/solaredgeport', 'w') as f:
-                f.write(str(port))
+            with open(file_path, 'w') as f:
+                f.write(ipaddress + ':' + str(port))
             return port
         # we were unable to read registers from port 502 so try 1502 next
     except ConnectionException:
         port = 1502
         client.port = port
-        resp= client.read_holding_registers(40206,5,unit=slaveid)
-        # now it worked, persist port number in ramdisk
-        if not ramdiskInitialized:
-            with open('/var/www/html/openWB/ramdisk/solaredgeport', 'w') as f:
-                f.write(str(port))
-            return port
+        try:
+            resp= client.read_holding_registers(40206,5,unit=slaveid)
+            # now it worked, persist port number in ramdisk
+            if not ramdiskInitialized:
+                with open(file_path, 'w') as f:
+                    f.write(ipaddress + ':' + str(port))
+                return port
+        except:
+            # no connection, remove cache file if any
+            if os.path.isfile(file_path):
+                os.remove(file_path)
     return port
